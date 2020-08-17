049cc39efc35ff5b94689972e487d6470f019557
daifuk_lana <59566441+daifuk-lana@users.noreply.github.com>
2020-08-17 19:12:07 +0900

    New Files

---
+++ README.md
@@ -0,0 +1,29 @@
+# 🎮ReplaceModelForEVMC4U
+![RM4EVMC4U_non](https://user-images.githubusercontent.com/59566441/90384044-ee50cb80-e0bb-11ea-8555-45a0806dffd7.png)  
+[EVMC4U](https://github.com/gpsnmeajp/EasyVirtualMotionCaptureForUnity)で何故かEnable Auto Load VRMオプションを使いたくない人向けのVRM切り替えスクリプトです。  
+注：バーチャルモーションキャプチャーで読み込むVRMとボーン構造が一致したVRMを使用して下さい。  
+　　具体的には、素体は同一で服だけ違うVRMに切り替えるなどに使用して下さい。
+ 
+# 🎙ご説明
+![RM4EVMC4U](https://user-images.githubusercontent.com/59566441/90384451-8353c480-e0bc-11ea-8c40-9b916a947e01.png)
+ 
+ReplaceModelForEVMC4Uを適当なGameObjectにアタッチして使用して下さい。  
+設定は画像の通りです。  
+ 
+* External Receiver：External ReceiverのGameObject指定  
+* Teleport Manager：Teleport ManagerのGameObject指定  
+* Models：VRMモデルの数、GameObject指定  
+* Model Transes Head：シネスイッチャー(Cinemachine)のLookAtに指定したいVRMモデルの数、ボーン指定  
+* V Cams：シネスイッチャー(Cinemachine)でLookAtを使用しているカメラの数、CM vcam指定  
+* Input Key：VRMモデルの数、切り替えキー指定  
+ 
+# 👽作者  
+ 
+* [大福らな](https://www.youtube.com/channel/UCtg9i4TxyddG5QV5CYZETiQ)
+* [pachelam.com](https://pachelam.com/)
+* [@daifuk_lana](https://twitter.com/daifuk_lana)
+ 
+# License
+ 
+"ReplaceModelForEVMC4U" is under [MIT license](https://en.wikipedia.org/wiki/MIT_License).
+ 
---
+++ ReplaceModelForEVMC4U.cs
@@ -0,0 +1,82 @@
+﻿using Cinemachine;
+using EVMC4U;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceModelForEVMC4U : MonoBehaviour
+{
+    public GameObject ExternalReceiver;
+    public GameObject TeleportManager;
+    public GameObject[] Models = new GameObject[2];
+    public Transform[] ModelTransesHead = new Transform[2];
+    public CinemachineVirtualCamera[] VCams = new CinemachineVirtualCamera[2];
+    public KeyCode[] InputKey = new KeyCode[2];
+
+    Animator[] ModelAnims;
+    Transform[] ModelTranses;
+    ExternalReceiver ReplaceModel;
+    TeleportManager MoveModel;
+    void Start()
+    {
+        ModelAnims = new Animator[Models.Length];
+        ModelTranses = new Transform[Models.Length];
+        if (ExternalReceiver != null) {
+            ReplaceModel = ExternalReceiver.GetComponent<ExternalReceiver>();
+        }
+        else {
+            Debug.LogError("ReplaceModel is null.");
+        }
+        if (TeleportManager != null)
+        {
+            MoveModel = TeleportManager.GetComponent<TeleportManager>();
+        }
+        else
+        {
+            Debug.Log("MoveModel is null.");
+        }
+        for (int i = 0; i < Models.Length; i++)
+        {
+            ModelAnims[i] = Models[i].GetComponent<Animator>();
+            ModelTranses[i] = Models[i].GetComponent<Transform>();
+            if (i != 0)
+            {
+                Models[i].SetActive(false);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < InputKey.Length; i++)
+        {
+            if (Input.GetKey(InputKey[i]))
+            {
+                ModelInput(i);
+            }
+        }
+    }
+    void ModelInput(int a)
+    {
+        for (int i = 0; i < InputKey.Length; i++)
+        {
+            if(i == a)
+            {
+                Models[a].SetActive(true);
+                ReplaceModel.Model = Models[a];
+                ReplaceModel.RootRotationTransform = ModelTranses[a];
+                ReplaceModel.RootPositionTransform = ModelTranses[a];
+                if(TeleportManager != null)
+                {
+                    MoveModel.model = ModelAnims[a];
+                }
+                for (int j = 0; j < VCams.Length; j++)
+                {
+                    VCams[j].LookAt = ModelTransesHead[a];
+                }
+            }
+            else
+            Models[i].SetActive(false);
+        }
+    }
+}
